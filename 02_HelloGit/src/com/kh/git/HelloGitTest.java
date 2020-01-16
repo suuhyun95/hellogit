166abd6fa52fee15494f9f3c95b74a159d0a5e6a
suuhyun95 <tngus6890@naver.com>
2020-01-16 19:12:31 +0900

    2번째 commit!!!
    저년먹고 commit!!

--- 02_HelloGit/src/com/kh/git/HelloGitTest.java
+++ 02_HelloGit/src/com/kh/git/HelloGitTest.java
@@ -5,6 +5,7 @@ public class HelloGitTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("안녕하세요!");
+		System.out.println("저녁맛있게 드셨어요?");
 	}
 
 }
