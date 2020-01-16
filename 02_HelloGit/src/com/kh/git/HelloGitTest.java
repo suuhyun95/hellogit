757ea2520452ea36555291e8530a9131b08260fa
suuhyun95 <tngus6890@naver.com>
2020-01-16 19:36:25 +0900

    remote repo 연결후 첫 commit & push!!!

--- 02_HelloGit/src/com/kh/git/HelloGitTest.java
+++ 02_HelloGit/src/com/kh/git/HelloGitTest.java
@@ -6,6 +6,7 @@ public class HelloGitTest {
 		// TODO Auto-generated method stub
 		System.out.println("안녕하세요!");
 		System.out.println("저녁맛있게 드셨어요?");
+		System.out.println("축하드립니다. commit& push!!!");
 	}
 
 }
